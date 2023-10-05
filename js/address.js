5c3ede2bf5fe1219cf1baf03dbf34cdd12ff76a9
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-10-05 15:38:50 +0700

    detail

--- js/address.js
+++ js/address.js
@@ -1,13 +1,12 @@
-
 function displayCity() {
     $.ajax({
         type: "GET",
         url: `http://localhost:8080/api/cities`,
         success: function (data) {
             let content = "<label for='select_city'>City</label><br>"
-             content += '<select id="select_city" onchange="displayDistrict()"  class="form-select">';
+            content += '<select id="select_city" onchange="displayDistrict()" style="width: 80%; height: 30px" class="form-select">';
             content += `<option>--Chọn thành phố-- </option>`;
-            for (let i = 0; i<data.length; i++) {
+            for (let i = 0; i < data.length; i++) {
 
                 content += `<option value = ${data[i].idCity}> ${data[i].name} </option>`;
             }
@@ -17,22 +16,112 @@ function displayCity() {
 
     })
 }
+
 function displayDistrict() {
-    let idCity = document.getElementById("select_city").value
+    let idCity = $('#select_city').val();
     $.ajax({
-        type: "GET",
+        type: "POST",
         url: `http://localhost:8080/api/addresses/city/${idCity}`,
         success: function (data) {
-            let content = '<select id="district" style="width: 80%; height: 30px" class="form-select">';
-            for (let i= 0; i < data.length; i++) {
+            let content = "<label for='select_district'>District</label><br>"
+            content += '<select id="select_district" style="width: 80%; height: 30px" class="form-select">';
+            for (let i = 0; i < data.length; i++) {
                 content += `<option value = ${data[i].idAddress}> ${data[i].name} </option>`;
             }
-            content +='</select>'
-            document.getElementById("select_district").innerHTML = content;
+            content += '</select>'
+            document.getElementById("district").innerHTML = content;
         }
     })
 }
+
+function displayStatus() {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/api/status`,
+        success: function (data) {
+            let content = "<label for='select_status'>Status</label><br>"
+            content += '<select id="select_status" style="width: 80%; height: 30px" class="form-select">';
+            content += `<option>--Chọn trạng thái-- </option>`;
+            for (let i = 0; i < data.length; i++) {
+                content += `<option value = ${data[i].idStatus}> ${data[i].name} </option>`;
+            }
+            content += '</select>'
+            document.getElementById("status").innerHTML = content;
+        }
+
+    })
+}
+
 function displayAddress() {
     displayCity();
     displayDistrict();
+    displayStatus()
+}
+
+function Filter() {
+    let minPrice = $("#minPrice").val();
+    let maxPrice = $("#maxPrice").val();
+    let count_bathroom = $("#bathroom").val();
+    let count_bedroom = $("#bedroom").val();
+    let idCity = $('#select_city').val();
+    let idDistrict = $('#select_district').val();
+    let idStatus = $('#select_status').val();
+    newFilter = {
+        minPrice: minPrice,
+        maxPrice: maxPrice,
+        count_bathroom: count_bathroom,
+        count_bedroom: count_bedroom,
+        city: {
+            idCity: idCity
+        },
+        address: {
+            idAddress: idDistrict
+        },
+        status: {
+            idStatus: idStatus
+        }
+    }
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        data: JSON.stringify(newFilter),
+        url: "http://localhost:8080/api/filters",
+        success: function (arr) {
+            if (arr == null) {
+                document.getElementById("homes").innerHTML = "Khong tim thay"
+            } else {
+                let content = ""
+                for (let i = 0; i < arr.length; i++) {
+                    content += `<div class="col-xl-3 col-lg-4 col-md-6">
+                 <div class="product-item">
+                    <div class="position-relative bg-light overflow-hidden">
+                        <img class="img-fluid w-100" src="../img/chien.jpg" alt="">
+                            <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
+                    </div>
+                    <div class="text-center p-4">
+                    <a class="d-block h5 mb-2" href="">${arr[i].name}</a>
+                    <span class="text-primary me-1">Gia: ${arr[i].price}|</span>
+                    <span class="text-body"> Phong ngu: ${arr[i].bedroom_count}</span>
+                    </div>
+                    <div class="d-flex border-top">
+                    <small class="w-50 text-center border-end py-2">
+                    <a class="text-body" href=""><i class="fa fa-eye text-primary me-2"></i>Xem chi tiết</a>
+                    </small>
+                    <small class="w-50 text-center py-2">
+                    <a class="text-body" href=""><i class="far fa-heart"></i>  Ưa thích</a>
+                    </small>
+                    </div>
+                </div>
+            </div>`
+                }
+                document.getElementById("homes").innerHTML = content
+            }
+        }
+
+    });
+    event.preventDefault()
 }
--- js/home.js
+++ js/home.js
@@ -52,25 +52,25 @@ function demoDisplay() {
         success: function (data) {
             arr = data
             let content = ""
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < arr.length; i++) {
                 content += `
 <div class="col-xl-3 col-lg-4 col-md-6">
     <div class="product-item">
         <div class="position-relative bg-light overflow-hidden">
-            <img class="img-fluid w-100" src="img/product-8.jpg" alt="">
+            <img class="img-fluid w-100" src="../img/chien.jpg" alt="">
             <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
         </div>
         <div class="text-center p-4">
-            <a class="d-block h5 mb-2" href="">Fresh Tomato</a>
-            <span class="text-primary me-1">$19.00</span>
-            <span class="text-body text-decoration-line-through">$29.00</span>
+            <a class="d-block h5 mb-2" href="">${arr[i].name}</a>
+            <span class="text-primary me-1">Gia: ${arr[i].price}|</span>
+            <span class="text-body"> Phong ngu: ${arr[i].bedroom_count}</span>
         </div>
         <div class="d-flex border-top">
             <small class="w-50 text-center border-end py-2">
-                <a class="text-body" href=""><i class="fa fa-eye text-primary me-2"></i>View detail</a>
+                <a class="text-body" href=""><i class="fa fa-eye text-primary me-2"></i>Xem chi tiết</a>
             </small>
             <small class="w-50 text-center py-2">
-                <a class="text-body" href=""><i class="fa fa-shopping-bag text-primary me-2"></i>Add to cart</a>
+                <a class="text-body" href=""><i class="far fa-heart"></i>  Ưa thích</a>
             </small>
         </div>
     </div>
@@ -79,4 +79,14 @@ function demoDisplay() {
             document.getElementById("homes").innerHTML = content
         }
     })
+}
+function detailHome(id) {
+    $.ajax({
+        url: `http://localhost:8080/api/homes/${id}`,
+        type:"GET"
+    })
+}
+
+function findOne() {
+
 }
