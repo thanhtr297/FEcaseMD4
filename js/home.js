c2458a76a29117fa5b0ce98c65c15694a3caa6ec
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-10-06 15:16:23 +0700

    phan trang

--- js/home.js
+++ js/home.js
@@ -1,85 +1,20 @@
-let arr;
-
-function displayAll1() {
-    $.ajax({
-        url: "http://localhost:8080/api/homes",
-        type: "GET",
-        success: function (data) {
-            arr = data
-            let content = `<h2>List home</h2>`
-            content += `<table><tr>
-                        <th>STT</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Bedroom</th>
-                        <th>Bathroom</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th>Image</th>
-                        <th>Status</th>
-                        <th>Account</th>
-                        <th colspan="2">Action</th>
-                        </tr>`
-            for (let i = 0; i < data.length; i++) {
-                content += `<tr>
-                        <td>${i + 1}</td>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].address}</td>
-                        <td>${data[i].bedroom_count}</td>
-                        <td>${data[i].bathroom_count}</td>
-                        <td>${data[i].description}</td>
-                        <td>${data[i].price}</td>
-<!--                        Chưa xử lý image//-->
-                        <td><img style="width: 100px; height: 100px" src="../src/main/resources/static/image/${data[i].image}" alt=""></td>
-
-                        <td>${data[i].status}</td>
-                        <td>${data[i].account}</td>
-                        
-                        <td><button onclick="updateProduct(${data[i].id})">Update</button></td>
-                        <td><button onclick="deleteProduct(${data[i].id})">Delete</button></td>
-                        </tr>`
-            }
-            content += `</table>`
-            document.getElementById("homes").innerHTML = content
-        }
-    })
-}
-
+let arrHome;
+let listDisplayPage;
+let numberPage;
+let totalPage;
 function demoDisplay() {
     $.ajax({
         url: "http://localhost:8080/api/homes",
         type: "GET",
         success: function (data) {
-            arr = data
-            let content = ""
-            for (let i = 0; i < arr.length; i++) {
-                content += `
-<div class="col-xl-3 col-lg-4 col-md-6">
-    <div class="product-item">
-        <div class="position-relative bg-light overflow-hidden">
-            <img class="img-fluid w-100" src="../img/chien.jpg" alt="">
-            <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
-        </div>
-        <div class="text-center p-4">
-            <span class="d-block h5 mb-2" href="">${arr[i].name}</span>
-            <span class="text-primary me-1">Giá: ${arr[i].price}</span>
-            <span class="text-body"> Phòng ngủ: ${arr[i].bedroom_count}</span>
-            <span class="text-body"> Phòng tắm: ${arr[i].bathroom_count}</span>
-        </div>
-        <div class="d-flex border-top">
-            <small class="w-50 text-center border-end py-2">
-                <a class="text-body" href=""><i class="fa fa-eye text-primary me-2"></i>Xem chi tiết</a>
-            </small>
-            <small class="w-50 text-center py-2">
-                <a class="text-body" href=""><i class="far fa-heart"></i>  Thuê ngay</a>
-            </small>
-        </div>
-    </div>
-</div>`
-            }
-            document.getElementById("homes").innerHTML = content
+            showHome(data)
+            numberPage = 0;
+            arrHome = data;
+            listDisplayPage = data.reverse();
+            showPage();
         }
     })
+
 }
 function detailHome(id) {
     $.ajax({
@@ -91,20 +26,121 @@ function detailHome(id) {
 function findOne() {
 
 }
+
+function Filter() {
+    let price = $("#price").val();
+    let priceValue = price.split("-")
+    let minPrice = priceValue[0];
+    let maxPrice = priceValue[1];
+    let count_bathroom = $("#bathroom").val();
+    let count_bedroom = $("#bedroom").val();
+    let idCity = $('#select_city').val();
+    if(idCity === "--Chọn thành phố--") {
+        idCity = null;
+    }
+    let idDistrict = $('#select_district').val();
+    if(idDistrict === undefined) {
+        idDistrict = null;
+    }
+    let idStatus = $('#select_status').val();
+    if(idStatus === "--Chọn trạng thái--") {
+        idStatus = null;
+    }
+    newFilter = {
+        minPrice: minPrice,
+        maxPrice: maxPrice,
+        count_bathroom: count_bathroom,
+        count_bedroom: count_bedroom,
+        city: {
+            idCity: idCity
+        },
+        address: {
+            idAddress: idDistrict
+        },
+        status: {
+            idStatus: idStatus
+        }
+    }
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        data: JSON.stringify(newFilter),
+        url: "http://localhost:8080/api/filters",
+        success: function (arr) {
+            if (arr == null) {
+                document.getElementById("homes").innerHTML = "Khong tim thay"
+            } else {
+                showHome(arr);
+                showPage();
+            }
+        }
+    });
+    event.preventDefault();
+}
+
 function searchByName() {
     let search = document.getElementById("search").value
     $.ajax({
         url: `http://localhost:8080/api/homes/search/${search}`,
         type: "GET",
         success: function (data) {
-            let content = ""
-            let arr = data
-            if (arr.EMPTY){
-                content= "Không có homestay nào phù hợp"
-                 return document.getElementById("homes").innerHTML=content
-            } else {
-                for (let i = 0; i < arr.length; i++) {
-                    content += `
+           showHome(data);
+            showPage();
+        }
+    })
+    event.preventDefault();
+}
+
+
+// phan trang
+
+
+function showPage() {
+    let data = listDisplayPage;
+    let elementPage = 2;
+    totalPage = Math.ceil(data.length / elementPage);
+    // numberPage;
+    //lưu numberPage ra biến Global
+    let startPage = (numberPage * elementPage);
+    let endPage = ((numberPage + 1) * elementPage);
+    let subArr = data.slice(startPage, endPage);
+    showHome(subArr);
+    showFootPage();
+    console.log(totalPage)
+}
+
+function showFootPage() {
+    let content = `<div id="footPage">
+                     <button class="btn btn-outline-primary" id="previous" onclick="previousPage(numberPage)">Previous</button>
+                     <span>${numberPage + 1}/${totalPage}</span>
+                     <button class="btn btn-outline-primary" id="next" onclick="nextPage(numberPage)">Next</button>
+                     </div>`
+    document.getElementById("footPage").innerHTML = content;
+    if (numberPage === 0){
+        $("#previous").hide();
+    }else if (numberPage === totalPage - 1){
+        $("#next").hide();
+    }
+}
+
+function previousPage(page) {
+    numberPage = page - 1;
+    showPage();
+}
+
+function nextPage(page) {
+    numberPage = page + 1;
+    showPage();
+}
+
+function showHome(data) {
+    let content = ""
+    for (let i = 0; i < data.length; i++) {
+        content += `
 <div class="col-xl-3 col-lg-4 col-md-6">
     <div class="product-item">
         <div class="position-relative bg-light overflow-hidden">
@@ -112,24 +148,21 @@ function searchByName() {
             <div class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">New</div>
         </div>
         <div class="text-center p-4">
-            <a class="d-block h5 mb-2" href="">${arr[i].name}</a>
-            <span class="text-primary me-1">Gia: ${arr[i].price}|</span>
-            <span class="text-body"> Phong ngu: ${arr[i].bedroom_count}</span>
+            <span class="d-block h5 mb-2" href="">${data[i].name}</span>
+            <span class="text-primary me-1">Giá: ${data[i].price}</span>
+            <span class="text-body"> Phòng ngủ: ${data[i].bedroom_count}</span>
+            <span class="text-body"> Phòng tắm: ${data[i].bathroom_count}</span>
         </div>
         <div class="d-flex border-top">
             <small class="w-50 text-center border-end py-2">
                 <a class="text-body" href=""><i class="fa fa-eye text-primary me-2"></i>Xem chi tiết</a>
             </small>
             <small class="w-50 text-center py-2">
-                <a class="text-body" href=""><i class="far fa-heart"></i> Thuê ngay</a>
+                <a class="text-body" href=""><i class="far fa-heart"></i>  Thuê ngay</a>
             </small>
         </div>
     </div>
 </div>`
-                }
-                document.getElementById("homes").innerHTML = content
-            }
-        }
-    })
-    event.preventDefault()
+    }
+    document.getElementById("homes").innerHTML = content
 }
